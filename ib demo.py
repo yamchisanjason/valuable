fde5403cb387e2ab7d8e9934923f3e4d277ece3d
yamchisanjason <98315798+yamchisanjason@users.noreply.github.com>
2023-07-10 17:34:45 +0800

    Add files via upload

--- ib demo.py
+++ ib demo.py
@@ -11,6 +11,14 @@ class IBapi(EWrapper, EClient):
         self.data = [] # to store the real-time market data
         self.start_time = datetime.utcnow().replace(hour=8, minute=0, second=0, microsecond=0)
         self.trade_executed = False
+        self.close_time = datetime.utcnow().replace(hour=17, minute=25, second=0, microsecond=0)
+        self.risk_reward_ratio = 2
+        self.account_balance = 10000000
+        self.take_profit_distance = None
+        self.stop_loss_distance = None
+        self.position_size = None
+        self.usd_balance = 0
+        self.eur_balance = 0
 
     def historicalData(self, reqId, bar):
         super().historicalData(reqId, bar)
@@ -30,11 +38,45 @@ class IBapi(EWrapper, EClient):
             if not self.trade_executed and datetime.utcnow() >= self.start_time + timedelta(minute=30):
                 high = max(self.data)
                 low = min(self.data)
-                if price > high:
-                    self.execute_trade('BUY', price)
-                elif price < low:
-                    self.execute_trade('SELL', price)
 
+                #calculate the stop loss and take profit levels based on the risk-reward ratio
+                distance = abs(high - low)
+                self.stop_loss_distance = distance / (1 + self.risk_reward_ratio)
+                self.take_profit_distance = self.stop_loss_distance * self.risk_reward_ratio
+
+                #calculate stop loss and take profit levels in price terms
+                if price > high:
+                    stop_loss = price - self.stop_loss_distance
+                    take_profit = price - self.take_profit_distance
+                    self.execute_trade('BUY', price, take_profit, stop_loss)
+
+                elif price < low:
+                    stop_loss = price + self.stop_loss_distance
+                    take_profit = price - self.take_profit_distance
+                    self.execute_trade('SELL', price, take_profit, stop_loss)
+
+            #check if the current time has passed the close time and close all open trades
+            if datetime.utcnow() >= self.close_time:
+                self.close_all_trades()
+
+        print(self.data)
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
+        super().orderStatus(orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId,whyHeld, mktCapPrice)
+
+        if status == 'Filled':
+            print(f'Order {orderId} has been filled')
+
+
+    def openOrder(self, orderId, contract, order, orderState):
+        super().openOrder(orderId, contract, order, orderState)
+        print(f'Order {orderId} has been opened')
+
+    def execDetails(self, reqId, contract, execution):
+        super().execDetails(reqId, contract, execution)
+        print(f'Trade executed: {execution.side} {execution.shares} shares of {contract.symbol}{contract.currency} at {execution.price}')
+        pnl = (execution.price - execution.avgPrice) * execution.shares
+        print(f'PnL: {pnl}')
 
     def nextValidId(self, orderId: int):
         super().nextValidId(orderId)
@@ -51,34 +93,109 @@ class IBapi(EWrapper, EClient):
         self.reqMktData(1, contract, "", False, False, []) # Request real-time market data
         self.reqHistoricalData(2, contract, "", "1 D", "1 min", "MIDPOINT", 0, 1, False, []) # Request historical data
 
+        #request account updates to synchronize the account balance
+        self.reqAccountUpdates(True, '')
+
+
+    def updateAccountValue(self, key: str, val: str, currency:str, accountName: str):
+        super().updateAccountValue(key, val, currency, accountName)
+        if key == 'CashBalance' and currency == 'USD':
+            self.usd_balance = float(val)
+        elif key == 'CashBalance'  and currency == 'EUR':
+            self.eur_balance = float(val)
+
+
+
+
+
+
     def stop(self):
         self.done = True
         self.disconnect()
 
-    def execute_trade(self, action, price):
+    def execute_trade(self, action, price, take_profit, stop_loss):
         contract = Contract()
         contract.symbol = 'EUR'
-        contract.exchange = 'IDDEALPRO'
+        contract.exchange = 'IDEALPRO'
         contract.currency = 'USD'
+
+        #calculate the position size based on the risk amount and stop loss distance
+        #risk_amount = self.account_balance * self.risk_percentage
+        self.position_size = int(self.account_balance / self.stop_loss_distance)
 
         order = Order()
         order.action = action
-        order.totalQuantity = 1000
+        order.totalQuantity = self.position_size
         order.orderType = 'MKT'
         order.transmit = True
+        order.account = 'DU7270972'
 
-        self.placeOrder(self.nextOrderId(), contract, order)    #replace with the next valid order ID
+        self.placeOrder(self.nextValidId(), contract, order)    #replace with the next valid order ID
         print(f'{action} order submitted for {contract.symbol}{contract.currency} at {price}')
+
+        #update the account balance based on the executed trade
+        if action == 'BUY':
+            self.usd_balance -= self.position_size
+            self.eur_balance += price * self.position_size
+
+        elif action == 'SELL':
+            self.account_balance += price * self.position_size
 
         self.trade_executed = True
 
+    def close_all_trades(self):
+        self.reqAllOpenOrders()     #request all open orders
+        open_orders = self.reqOpenOrders()
+        for order in open_orders:
+            #execute a trade in the opposite direction of the open trades
+            if order.action == 'BUY':
+                action = 'SELL'
+                price = self.data[-1]   #current market price
+
+            elif order.action == 'SELL':
+                action = 'BUY'
+                price = self.data[-1]   #current market price
+
+            else:
+                #this order is not a trade order, skip it
+                continue
+
+            #place the opposite trade at the current market price
+            contract = Contract()
+            contract.symbol = 'EUR'
+            contract.exchange = 'IDEALPRO'
+            contract.currency = 'USD'
+
+            order = Order()
+            order.action = action
+            order.totalQuantity = self.position_size
+            order.orderType = 'MKT'
+            order.transmit = True
+            order.account = 'DU7270972'
+
+            self.placeOrder(self.nextValidId(), contract, order)
+            print(f'{action} order submitted for {contract.symbol}{contract.currency} at {price}')
+
+            #update the account balance based on the executed trade
+            if action == 'BUY':
+                self.usd_balance -= self.position_size
+                self.eur_balance += price * self.position_size
+
+            elif action == 'SELL':
+                self.usd_balance += self.position_size
+                self.eur_balance -= price * self.position_size
+
+            print(f'Updated account balance: USD {self.usd_balance:.2f}, EUR {self.eur_balance:.2f}')
+
+        self.trade_executed = False
 
 
 
+
+app = IBapi()
+app.connect("127.0.0.1", 7497, 0)
+app.start()
 
 while True:
-
-    app = IBapi()
-    app.connect("127.0.0.1", 7497, 0)  # Port number may vary depending on your setup
     app.run()
 
